b57eeb60b56f6a510d5145e81c5e86b6992311bb
Tatyana2121 <gia-celena@rambler.ru>
2023-04-06 21:45:27 +0300

    add form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 //burger
 $('.burger').on('click', function (e) {
   e.preventDefault()
@@ -5,3 +6,27 @@ $('.burger').on('click', function (e) {
   $('.header').toggleClass('burger--active')
 })
 
+//feedback-form
+const feedbackForm = document.querySelector('.form-inner'),
+      headerBtn = document.querySelector('.header__body-btn'),
+      overlay = document.querySelector('.overlay'),
+      feedbackBtn = document.querySelector('.feedback-form__btn'),
+      thanks = document.querySelector('.thanks')
+headerBtn.addEventListener('click', function () {
+  overlay.classList.toggle("hidden")
+  feedbackForm.classList.toggle('hidden')
+})
+// feedbackBtn.addEventListener('click', function () {
+//   thanks.classList.toggle('hidden-thanks')
+//   feedbackForm.classList.toggle('hidden')
+// })
+overlay.addEventListener("click", function () {
+  overlay.classList.toggle("hidden");
+  feedbackForm.classList.toggle('hidden')
+});
+document.addEventListener('keydown', function (event) {
+  if (event.key == "Escape" && !feedbackForm.classList.contains('hidden')) {
+    overlay.classList.toggle("hidden");
+    feedbackForm.classList.toggle('hidden')
+  }
+})
