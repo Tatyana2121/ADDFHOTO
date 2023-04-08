b7af977f273c8f9db61f51059f020e7e1e35b529
Tatyana2121 <gia-celena@rambler.ru>
2023-04-08 17:23:36 +0300

    corrected feedback form

--- js/main.js
+++ js/main.js
@@ -11,23 +11,45 @@ const feedbackForm = document.querySelector('.form-inner'),
       headerBtn = document.querySelector('.header__body-btn'),
       overlay = document.querySelector('.overlay'),
       feedbackBtn = document.querySelector('.feedback-form__btn'),
-      thanks = document.querySelector('.thanks')
+      close = document.querySelector('.form-close'),
+      thanks = document.querySelector('.thanks'),
+      thanksClose = document.querySelector('#thanks-close-btn')
       
 headerBtn.addEventListener('click', function () {
   overlay.classList.toggle("hidden")
   feedbackForm.classList.toggle('hidden')
 })
-// feedbackBtn.addEventListener('click', function () {
-//   thanks.classList.toggle('hidden-thanks')
-//   feedbackForm.classList.toggle('hidden')
-// })
-overlay.addEventListener("click", function () {
-  overlay.classList.toggle("hidden");
-  feedbackForm.classList.toggle('hidden')
-});
 document.addEventListener('keydown', function (event) {
   if (event.key == "Escape" && !feedbackForm.classList.contains('hidden')) {
-    overlay.classList.toggle("hidden");
+    overlay.classList.toggle("hidden")
     feedbackForm.classList.toggle('hidden')
   }
 })
+close.addEventListener("click", function () {
+  overlay.classList.toggle("hidden")
+  feedbackForm.classList.toggle('hidden')
+});
+feedbackBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  thanks.classList.toggle('hidden-thanks')
+  feedbackForm.classList.toggle('hidden')
+})
+document.addEventListener('keydown', function (event) {
+  if (event.key == "Escape" && !thanks.classList.contains('hidden-thanks')) {
+    overlay.classList.toggle("hidden")
+    thanks.classList.toggle('hidden-thanks')
+  }
+})
+thanksClose.addEventListener("click", function () {
+  overlay.classList.toggle("hidden")
+  thanks.classList.toggle('hidden-thanks')
+});
+overlay.addEventListener("click", function () {
+  if(feedbackForm.classList.contains('hidden') && !thanks.classList.contains('hidden-thanks')) {
+    overlay.classList.toggle("hidden")
+    thanks.classList.toggle('hidden-thanks')
+  } else {
+    overlay.classList.toggle("hidden")
+    feedbackForm.classList.toggle('hidden')
+  }  
+});
